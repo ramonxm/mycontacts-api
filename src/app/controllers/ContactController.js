0688ac5e2318df2ee9618bb6adcde5380f9867d4
Ramon Xavier <ramon1234rxm@gmail.com>
2021-09-13 10:45:55 -0300

    feat: update method show in contact controller

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -6,8 +6,15 @@ class ContactController {
     response.json(contacts);
   }
 
-  show() {
-    // Obter UM registro
+  async show(request, response) {
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(contact);
   }
 
   store() {
