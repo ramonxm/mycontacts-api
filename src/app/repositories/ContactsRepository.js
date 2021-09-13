48a95c82b383ac3689754e5e6102c1ff3eb038f2
Ramon Xavier <ramon1234rxm@gmail.com>
2021-09-13 10:45:29 -0300

    feat: method findbyid

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,18 +1,31 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Ramon',
     email: 'ramon@mail.com',
     phone: '(21) XXXX-XXX',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'José',
+    email: 'ramon@mail.com',
+    phone: '(21) XXXX-XXX',
+    category_id: v4(),
   },
 ];
 
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
   }
 }
 
