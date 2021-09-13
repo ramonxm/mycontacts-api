1cac7423ce3bb8128849d0ac05df36eba3221c0d
Ramon Xavier <ramon1234rxm@gmail.com>
2021-09-13 10:01:00 -0300

    feat: update server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
+app.use(routes);
 
-app.get('/', (request, response) => {
-  response.send('Hello World');
-});
-
-app.listen(3000, () => console.log('Servert started at htpp://localhost:3000'));
+app.listen(3000, () => console.log('Server started at htpp://localhost:3000'));
