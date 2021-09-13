227ba86dc1a007790849ce7e100007913ee94283
Ramon Xavier <ramon1234rxm@gmail.com>
2021-09-13 14:10:40 -0300

    feat: delete in contact controller

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -25,8 +25,17 @@ class ContactController {
     // Editar um registro
   }
 
-  delete() {
-    // Deletar um registro
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    await ContactsRepository.delete(id);
+    response.sendStatus(204);
   }
 }
 
