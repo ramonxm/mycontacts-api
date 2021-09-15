920ed8917ed8f9caea3fbf87f0305d05ef98899d
Ramon Xavier <ramon1234rxm@gmail.com>
2021-09-15 19:02:13 -0300

    feat: create a find-by-id, find-by-name and delete

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -23,15 +23,36 @@ class ContactsRepository {
   }
 
   findById(id) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id),
-    ));
+    return new Promise((resolve) => resolve(contacts.find((contact) => contact.id === id)));
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => resolve(contacts.find((contact) => contact.email === email)));
   }
 
   delete(id) {
     return new Promise((resolve) => {
       contacts = contacts.filter((contact) => contact.id !== id);
       resolve();
+    });
+  }
+
+  create({
+    name,
+    email,
+    phone,
+    category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts.push(newContact);
+      resolve(newContact);
     });
   }
 }
