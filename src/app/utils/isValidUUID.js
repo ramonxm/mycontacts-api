63fe94569ef38c952746468211013a7d80c70e72
Ramon <ramon1234rxm@gmail.com>
2022-05-23 14:51:01 -0300

    feat: add isvaliduuid

---
+++ src/app/utils/isValidUUID.js
@@ -0,0 +1,6 @@
+function isValidUUID(string) {
+  const regex = /ˆ[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b--[0-9a-fA-F]{12}$/gi;
+  return regex.test(string);
+}
+
+module.exports = isValidUUID;
